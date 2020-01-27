fb32458cd34be44605d4f9c74dfa984dfb7b4ee8
kennyfong19931 <kennyfong19931@gmail.com>
2020-01-27 23:54:42 +0800

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,94 @@
+<script>
+  import { stores } from "@sapper/app";
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+
+  import AppBar from "components/AppBar";
+  import Tabs from "components/Tabs";
+  import Button from "components/Button";
+  import { Spacer } from "components/Util";
+  import List from "components/List";
+  import ListItem from "components/List/ListItem.svelte";
+  import NavigationDrawer from "components/NavigationDrawer";
+  import ProgressLinear from "components/ProgressLinear";
+  import { writable } from "svelte/store";
+
+  const right = writable(false);
+  const persistent = writable(true);
+  const elevation = writable(false);
+  const showNav = writable(true);
+
+
+  const { preloading, page } = stores();
+
+  let selected = "";
+
+  $: path = $page.path;
+
+  const menu = [{ to: "/about", text: "About" }, { to: "/blog", text: "Blog" }];
+
+  const topMenu = [
+    { to: "/about", text: "About" },
+    { to: "/blog", text: "Blog" }
+  ];
+</script>
+
+{#each menu as link}
+  <a href={link.to} class="hidden">{link.text}</a>
+{/each}
+
+<svelte:head>
+  <title>PS Plus Instant game collection</title>
+</svelte:head>
+
+{#if $preloading}
+  <ProgressLinear app />
+{/if}
+
+<AppBar>
+  <a href="." class="px-2 md:px-8 flex items-center">
+    <img src="/logo.png" alt="Smelte logo" width="44" />
+    <h6 class="pl-3 text-white tracking-widest font-thin text-lg">PS Plus Instant game collection</h6>
+  </a>
+  <Spacer />
+  <Tabs navigation items={topMenu} bind:selected={path} />
+  <div class="md:hidden">
+    <Button
+      icon="menu"
+      small
+      flat
+      add="text-white"
+      remove="p-1 h-4 w-4"
+      iconClasses={i => i.replace('p-4', 'p-3').replace('m-4', 'm-3')}
+      text
+      on:click={() => showNav.set(!$showNav)} />
+  </div>
+</AppBar>
+
+<main
+  class="container relative p-8 lg:max-w-3xl lg:ml-64 mx-auto mb-10 mt-24
+  md:ml-56 md:max-w-md md:px-3"
+  transition:fade={{ duration: 300 }}>
+  <NavigationDrawer
+    bind:show={$showNav}
+    right={$right}
+    persistent={$persistent}
+    elevation={$elevation}
+  >
+    <h6 class="p-6 ml-1 pb-2 text-xs text-gray-900">Menu</h6>
+    <List items={menu}>
+      <span slot="item" let:item class="cursor-pointer">
+        <a href={item.to}>
+          <ListItem
+            selected={path.includes(item.to)}
+            {...item}
+            dense
+            navigation />
+        </a>
+      </span>
+    </List>
+    <hr />
+  </NavigationDrawer>
+
+  <slot />
+</main>
