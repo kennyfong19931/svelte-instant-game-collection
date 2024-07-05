b745571c83c9caa3ee75ee2be5c39731aaa558c0
kennyfong19931 <kennyfong19931@users.noreply.github.com>
2024-07-05 15:06:30 +0000

    deploy: 3ce692b601b6c42ccb46dc56839886a8e390a257

--- _app/immutable/chunks/paths.56259154.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1bjcs2k)==null?void 0:s.base)??"/svelte-instant-game-collection";var e;const a=((e=globalThis.__sveltekit_1bjcs2k)==null?void 0:e.assets)??t;export{a,t as b};
